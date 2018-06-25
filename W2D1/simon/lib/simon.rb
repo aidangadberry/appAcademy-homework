60eb39fd675009c74314f0effc188a063c5a095b
Aidan Gadberry <agadberr@gmail.com>
2018-06-24 17:13:17 -0700

    Pass all specs

--- W2D1/simon/lib/simon.rb
+++ W2D1/simon/lib/simon.rb
@@ -4,38 +4,65 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
+    until @game_over
+      take_turn
+    end
 
+    game_over_message
+    reset_game
   end
 
   def take_turn
+    show_sequence
+    require_sequence
 
+    return if @game_over
+
+    round_success_message
+    @sequence_length += 1
   end
 
   def show_sequence
-
+    add_random_color
+    p @seq
+    sleep(2)
+    system("clear")
   end
 
   def require_sequence
-
+    puts "Enter the sequence, one color at a time"
+    @seq.each do |el|
+      input = gets.chomp
+      next if input == el
+      @game_over = true
+      break
+    end
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
-
+    puts "You completed a sequence of length #{sequence_length}"
   end
 
   def game_over_message
-
+    puts "Game over. Your longest sequence was #{sequence_length - 1}"
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
+
+simon = Simon.new
+simon.play
