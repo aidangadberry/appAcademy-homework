31091a06233a4407391e26c3e28e50c96ba8e839
Aidan Gadberry <agadberr@gmail.com>
2018-07-10 22:42:25 -0700

    Add auth methods to User model

--- W4D3/rails_auth/app/models/user.rb
+++ W4D3/rails_auth/app/models/user.rb
@@ -10,12 +10,41 @@
 #  updated_at      :datetime         not null
 #
 
+require 'securerandom'
+
 class User < ApplicationRecord
   validates :username, :session_token, uniqueness: true, presence: true
   validates :password_digest, presence: { message: "Password can't be blank" }
   validates :password, length: { minimum: 6, allow_nil: true }
   before_validation :ensure_session_token
 
+  attr_reader :password
 
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+      user
+    else
+      nil
+    end
+  end
 
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64[0...16]
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token = User.generate_session_token if self.session_token.nil?
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 end
